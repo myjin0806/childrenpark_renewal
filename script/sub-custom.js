2825053fae23b0673a55f780aad2ae240f795222
Kim Haejin <myjin0806@gmail.com>
2024-12-04 17:29:36 +0900

    Update sub-custom.js

--- script/sub-custom.js
+++ script/sub-custom.js
@@ -117,51 +117,34 @@ function scrollToSection(index) {
 }
 
 /* #### directions.html #### */
-//지도 구현
-var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
-var mapOption = { 
-    center: new kakao.maps.LatLng(37.55146818894127, 127.0837919213508), // 지도의 중심좌표
-    level: 2 // 지도의 확대 레벨
+// 카카오 지도 API를 비동기적으로 로드하는 스크립트
+var script = document.createElement('script');
+script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=7560344f7149ee3ecede4f1d58649e54&autoload=false";
+script.async = true; // 비동기 로드
+script.onload = function() {
+    // 스크립트 로드가 완료되면 지도 생성 및 기타 작업을 수행
+    initializeMap();
 };
+document.head.appendChild(script);
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+// 지도 초기화 함수
+function initializeMap() {
+    // 지도를 표시할 div와 기본 옵션 설정
+    var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
+    var mapOption = {
+        center: new kakao.maps.LatLng(37.55146818894127, 127.0837919213508), // 지도의 중심 좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
-// 지도를 클릭한 위치에 표출할 마커입니다
-var marker = new kakao.maps.Marker({ 
-    // 지도 중심좌표에 마커를 생성합니다 
-    position: map.getCenter() 
-}); 
-// 지도에 마커를 표시합니다
-marker.setMap(map);
+    // 지도를 생성
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 지도에 클릭 이벤트를 등록합니다
-// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
-    // 클릭한 위도, 경도 정보를 가져옵니다 
-    var latlng = mouseEvent.latLng; 
-    
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-    
-    // 메시지 출력 포맷을 수정하여 위도, 경도를 깔끔하게 표시
-    var message = '클릭한 위치의 위도: ' + latlng.getLat() + ', 경도: ' + latlng.getLng();
-    
-    // 클릭한 위치의 위도, 경도를 HTML에 표시
-    var resultDiv = document.getElementById('clickLatlng'); 
-    resultDiv.innerHTML = message;
-});
-
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-var mapTypeControl = new kakao.maps.MapTypeControl();
-
-// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-// 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성합니다
-var zoomControl = new kakao.maps.ZoomControl();
-map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
+    // 지도에 마커를 표시할 예제
+    var marker = new kakao.maps.Marker({
+        position: map.getCenter() // 지도 중심에 마커 위치 지정
+    });
+    marker.setMap(map);
+}
 
 //탭기능 구현
 document.addEventListener('DOMContentLoaded', function() {
