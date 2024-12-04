ff4adeea5064bd9352e3b1affe1e02c5808253fc
Kim Haejin <myjin0806@gmail.com>
2024-12-04 17:25:35 +0900

    Update sub-custom.js

--- script/sub-custom.js
+++ script/sub-custom.js
@@ -118,11 +118,11 @@ function scrollToSection(index) {
 
 /* #### directions.html #### */
 //지도 구현
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(37.55146818894127, 127.0837919213508), // 지도의 중심좌표
-        level: 2 // 지도의 확대 레벨
-    };
+var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
+var mapOption = { 
+    center: new kakao.maps.LatLng(37.55146818894127, 127.0837919213508), // 지도의 중심좌표
+    level: 2 // 지도의 확대 레벨
+};
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
@@ -137,21 +137,19 @@ marker.setMap(map);
 // 지도에 클릭 이벤트를 등록합니다
 // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
 kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
-    
     // 클릭한 위도, 경도 정보를 가져옵니다 
     var latlng = mouseEvent.latLng; 
     
     // 마커 위치를 클릭한 위치로 옮깁니다
     marker.setPosition(latlng);
     
-    var message = '37.55146818894127' + latlng.getLat() + ' 이고, ';
-    message += '127.0837919213508 ' + latlng.getLng() + ' 입니다';
+    // 메시지 출력 포맷을 수정하여 위도, 경도를 깔끔하게 표시
+    var message = '클릭한 위치의 위도: ' + latlng.getLat() + ', 경도: ' + latlng.getLng();
     
+    // 클릭한 위치의 위도, 경도를 HTML에 표시
     var resultDiv = document.getElementById('clickLatlng'); 
     resultDiv.innerHTML = message;
-    
 });
-
 
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 var mapTypeControl = new kakao.maps.MapTypeControl();
@@ -160,9 +158,10 @@ var mapTypeControl = new kakao.maps.MapTypeControl();
 // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+// 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성합니다
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
 
 //탭기능 구현
 document.addEventListener('DOMContentLoaded', function() {
